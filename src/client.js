fd5e2c692fb0da2e45ab649b7ac95fd973c80f99
Boris Smirnov <bsmirnov@rvision.ru>
2023-03-10 00:26:35 +0300

    hotfix connection

--- src/client.js
+++ src/client.js
@@ -1,10 +1,10 @@
-const WebSocket = require('ws');
+const WebSocket = require('ws')
 
-const misc = require('./miscRequests');
-const protocol = require('./protocol');
+const misc = require('./miscRequests')
+const protocol = require('./protocol')
 
-const quoteSessionGenerator = require('./quote/session');
-const chartSessionGenerator = require('./chart/session');
+const quoteSessionGenerator = require('./quote/session')
+const chartSessionGenerator = require('./chart/session')
 
 /**
  * @typedef {Object} Session
@@ -19,7 +19,7 @@ const chartSessionGenerator = require('./chart/session');
  * @param {string} t Packet type
  * @param {string[]} p Packet data
  * @returns {void}
-*/
+ */
 
 /**
  * @typedef {Object} ClientBridge
@@ -36,22 +36,22 @@ const chartSessionGenerator = require('./chart/session');
 
 /** @class */
 module.exports = class Client {
-  #ws;
+  #ws
 
-  #logged = false;
+  #logged = false
 
   /** If the client is logged in */
   get isLogged() {
-    return this.#logged;
+    return this.#logged
   }
 
   /** If the cient was closed */
   get isOpen() {
-    return this.#ws.readyState === this.#ws.OPEN;
+    return this.#ws.readyState === this.#ws.OPEN
   }
 
   /** @type {SessionList} */
-  #sessions = {};
+  #sessions = {}
 
   #callbacks = {
     connected: [],
@@ -62,20 +62,20 @@ module.exports = class Client {
 
     error: [],
     event: [],
-  };
+  }
 
   /**
    * @param {ClientEvent} ev Client event
    * @param {...{}} data Packet data
    */
   #handleEvent(ev, ...data) {
-    this.#callbacks[ev].forEach((e) => e(...data));
-    this.#callbacks.event.forEach((e) => e(ev, ...data));
+    this.#callbacks[ev].forEach(e => e(...data))
+    this.#callbacks.event.forEach(e => e(ev, ...data))
   }
 
   #handleError(...msgs) {
-    if (this.#callbacks.error.length === 0) console.error(...msgs);
-    else this.#handleEvent('error', ...msgs);
+    if (this.#callbacks.error.length === 0) console.error(...msgs)
+    else this.#handleEvent('error', ...msgs)
   }
 
   /**
@@ -84,7 +84,7 @@ module.exports = class Client {
    * @event onConnected
    */
   onConnected(cb) {
-    this.#callbacks.connected.push(cb);
+    this.#callbacks.connected.push(cb)
   }
 
   /**
@@ -93,7 +93,7 @@ module.exports = class Client {
    * @event onDisconnected
    */
   onDisconnected(cb) {
-    this.#callbacks.disconnected.push(cb);
+    this.#callbacks.disconnected.push(cb)
   }
 
   /**
@@ -115,7 +115,7 @@ module.exports = class Client {
    * @event onLogged
    */
   onLogged(cb) {
-    this.#callbacks.logged.push(cb);
+    this.#callbacks.logged.push(cb)
   }
 
   /**
@@ -124,7 +124,7 @@ module.exports = class Client {
    * @event onPing
    */
   onPing(cb) {
-    this.#callbacks.ping.push(cb);
+    this.#callbacks.ping.push(cb)
   }
 
   /**
@@ -133,7 +133,7 @@ module.exports = class Client {
    * @event onData
    */
   onData(cb) {
-    this.#callbacks.data.push(cb);
+    this.#callbacks.data.push(cb)
   }
 
   /**
@@ -142,7 +142,7 @@ module.exports = class Client {
    * @event onError
    */
   onError(cb) {
-    this.#callbacks.error.push(cb);
+    this.#callbacks.error.push(cb)
   }
 
   /**
@@ -151,63 +151,66 @@ module.exports = class Client {
    * @event onEvent
    */
   onEvent(cb) {
-    this.#callbacks.event.push(cb);
+    this.#callbacks.event.push(cb)
   }
 
   #parsePacket(str) {
-    if (!this.isOpen) return;
+    if (!this.isOpen) return
 
-    protocol.parseWSPacket(str).forEach((packet) => {
-      if (global.TW_DEBUG) console.log('§90§30§107 CLIENT §0 PACKET', packet);
-      if (typeof packet === 'number') { // Ping
-        this.#ws.send(protocol.formatWSPacket(`~h~${packet}`));
-        this.#handleEvent('ping', packet);
-        return;
+    protocol.parseWSPacket(str).forEach(packet => {
+      if (global.TW_DEBUG) console.log('§90§30§107 CLIENT §0 PACKET', packet)
+      if (typeof packet === 'number') {
+        // Ping
+        this.#ws.send(protocol.formatWSPacket(`~h~${packet}`))
+        this.#handleEvent('ping', packet)
+        return
       }
 
-      if (packet.m === 'protocol_error') { // Error
-        this.#handleError('Client critical error:', packet.p);
-        this.#ws.close();
-        return;
+      if (packet.m === 'protocol_error') {
+        // Error
+        this.#handleError('Client critical error:', packet.p)
+        this.#ws.close()
+        return
       }
 
-      if (packet.m && packet.p) { // Normal packet
+      if (packet.m && packet.p) {
+        // Normal packet
         const parsed = {
           type: packet.m,
           data: packet.p,
-        };
+        }
 
-        const session = packet.p[0];
+        const session = packet.p[0]
 
         if (session && this.#sessions[session]) {
-          this.#sessions[session].onData(parsed);
-          return;
+          this.#sessions[session].onData(parsed)
+          return
         }
       }
 
       if (!this.#logged) {
-        this.#handleEvent('logged', packet);
-        return;
+        this.#handleEvent('logged', packet)
+        return
       }
 
-      this.#handleEvent('data', packet);
-    });
+      this.#handleEvent('data', packet)
+    })
   }
 
-  #sendQueue = [];
+  #sendQueue = []
 
   /** @type {SendPacket} Send a custom packet */
   send(t, p = []) {
-    this.#sendQueue.push(protocol.formatWSPacket({ m: t, p }));
-    this.sendQueue();
+    this.#sendQueue.push(protocol.formatWSPacket({ m: t, p }))
+    this.sendQueue()
   }
 
   /** Send all waiting packets */
   sendQueue() {
     while (this.isOpen && this.#logged && this.#sendQueue.length > 0) {
-      const packet = this.#sendQueue.shift();
-      this.#ws.send(packet);
-      if (global.TW_DEBUG) console.log('§90§30§107 > §0', packet);
+      const packet = this.#sendQueue.shift()
+      this.#ws.send(packet)
+      if (global.TW_DEBUG) console.log('§90§30§107 > §0', packet)
     }
   }
 
@@ -222,66 +225,73 @@ module.exports = class Client {
    * @param {ClientOptions} clientOptions TradingView client options
    */
   constructor(clientOptions = {}) {
-    if (clientOptions.DEBUG) global.TW_DEBUG = clientOptions.DEBUG;
+    if (clientOptions.DEBUG) global.TW_DEBUG = clientOptions.DEBUG
 
-    const server = clientOptions.server || 'data';
-    this.#ws = new WebSocket(`wss://${server}.tradingview.com/socket.io/websocket`, {
+    const server = clientOptions.server || 'data'
+    this.#ws = new WebSocket(`wss://${server}.tradingview.com/socket.io/websocket?&type=chart`, {
       origin: 'https://s.tradingview.com',
-    });
+    })
 
     if (clientOptions.token) {
-      misc.getUser(clientOptions.token).then((user) => {
-        this.#sendQueue.unshift(protocol.formatWSPacket({
-          m: 'set_auth_token',
-          p: [user.authToken],
-        }));
-        this.#logged = true;
-        this.sendQueue();
-      }).catch((err) => {
-        this.#handleError('Credentials error:', err.message);
-      });
+      misc
+        .getUser(clientOptions.token)
+        .then(user => {
+          this.#sendQueue.unshift(
+            protocol.formatWSPacket({
+              m: 'set_auth_token',
+              p: [user.authToken],
+            })
+          )
+          this.#logged = true
+          this.sendQueue()
+        })
+        .catch(err => {
+          this.#handleError('Credentials error:', err.message)
+        })
     } else {
-      this.#sendQueue.unshift(protocol.formatWSPacket({
-        m: 'set_auth_token',
-        p: ['unauthorized_user_token'],
-      }));
-      this.#logged = true;
-      this.sendQueue();
+      this.#sendQueue.unshift(
+        protocol.formatWSPacket({
+          m: 'set_auth_token',
+          p: ['unauthorized_user_token'],
+        })
+      )
+      this.#logged = true
+      this.sendQueue()
     }
 
     this.#ws.on('open', () => {
-      this.#handleEvent('connected');
-      this.sendQueue();
-    });
+      this.#handleEvent('connected')
+      this.sendQueue()
+    })
 
     this.#ws.on('close', () => {
-      this.#logged = false;
-      this.#handleEvent('disconnected');
-    });
+      this.#logged = false
+      this.#handleEvent('disconnected')
+    })
 
-    this.#ws.on('message', (data) => this.#parsePacket(data));
+    this.#ws.on('message', data => this.#parsePacket(data))
   }
 
   /** @type {ClientBridge} */
   #clientBridge = {
     sessions: this.#sessions,
     send: (t, p) => this.send(t, p),
-  };
+  }
 
   /** @namespace Session */
   Session = {
     Quote: quoteSessionGenerator(this.#clientBridge),
     Chart: chartSessionGenerator(this.#clientBridge),
-  };
+  }
 
   /**
    * Close the websocket connection
    * @return {Promise<void>} When websocket is closed
    */
   end() {
-    return new Promise((cb) => {
-      if (this.#ws.readyState) this.#ws.close();
-      cb();
-    });
+    return new Promise(cb => {
+      if (this.#ws.readyState) this.#ws.close()
+      cb()
+    })
   }
-};
+}
